bcd995a4833c687082e947b5c928d8e533ed6073
piratinn <joukrxxx@gmail.com>
2023-12-31 21:53:35 +0100

    add delete function

--- queue_priority.c
+++ queue_priority.c
@@ -16,30 +16,14 @@ int main(int argc, char *argv[])
         scanf("%d", &option);
         switch(option)
         {
-            case 1: START = create_ll(START);
-                if (START != NULL)
-                    printf("list created succesfully\n");
+            case 1: START = display(START);
                 break;
-            case 2: START = display(START);
+            case 2: START = insert(START);  
                 break;
-            case 3: START = add_node_bg(START);
+            case 3: START = delete(START);
                 break;
-            case 4: START = add_node_end(START);
-                break;
-            case 5: START = insert_before(START);
-                break;
-            case 6: START = insert_after(START);
-                break;
-            case 7: START = delete_first(START);
-                break;
-            case 8: START = delete_selected(START);
-                break;
-            case 9: START = delete_last(START);
-                break;
-            case 10: START = ascending_node(START);
-                break;
-            case 11: START = insert(START);        }
-    } while (option != 12);
+            }
+    } while (option != 4);
     free(START);
     return 0;
 }
@@ -47,48 +31,12 @@ int main(int argc, char *argv[])
 void help(void)
 {
     printf("\n*****linked list menu********\n\n");
-    printf("\tPress 1 to create a list\n");
-    printf("\tPress 2 to display all node\n");
-    printf("\tPress 3 to add a node at top of list\n");
-    printf("\tPress 4 to add node at last of list\n");
-    printf("\tPress 5 to insert node before node looking for\n");
-    printf("\tPress 6 to insert node after node looking for\n");
-    printf("\tPress 7 to delete first node\n");
-    printf("\tPress 8 to delete selected node\n");
-    printf("\tPress 9 to delete last node\n");
-    printf("\tPress 10 to change order of data node");
-    printf("\tPress 11 to insert new node in Queue\n");
-    printf("\tPress 12 to exit\n");
+    printf("\tPress 1 to display all node\n");
+    printf("\tPress 2 to insert new node in Queue\n");
+    printf("\tPress 3 to delete node\n");
+    printf("\tPress 4 to exit\n");
 }
 
-node_t *create_ll(node_t *start)
-{
-    node_t *newNode;
-    node_t *ptr;
-    int data, year, month, day, hours, minutes;
-    printf("enter  year, month, day, hours, minute or press -1 to end\n");
-    scanf("%d%d%d%d%d", &year, &month, &day, &hours, &minutes);
-    while(year != -1 && month != -1 && day != -1)
-    {
-        newNode = (node_t*)calloc(1, sizeof(node_t));
-        newNode->Ttime = get_rawtime(year, month, day, hours, minutes);
-        if (start == NULL) {
-            newNode->next = NULL;
-            newNode->prev = NULL;
-            start = newNode;
-        } else {
-            ptr = start;
-            while(ptr->next != NULL)
-                ptr = ptr->next;
-            ptr->next = newNode;
-            newNode->prev = ptr;
-            newNode->next = NULL;
-        }
-        printf("enter  year, month, day, hours, minute or press -1 to end\n");
-        scanf("%d%d%d%d%d", &year, &month, &day, &hours, &minutes);
-    }
-    return start;
-}
 
 node_t *display(node_t *start)
 {
@@ -100,113 +48,6 @@ node_t *display(node_t *start)
         ptr = ptr->next;
         counter++;
     }
-    return start;
-}
-
-node_t *add_node_bg(node_t *start)
-{
-    int data;
-    printf("enter node data\n");
-    scanf("%d",&data);
-    node_t *newNode = (node_t*)calloc(1,sizeof(node_t));
-    newNode->data = data;
-    newNode->prev = NULL;
-    start->prev = newNode;
-    newNode->next = start;
-    start = newNode;
-    return start;
-}
-node_t *add_node_end(node_t *start)
-{
-    int data;
-    printf("enter node data \n");
-    scanf("%d", &data);
-    node_t *newNode = (node_t*)calloc(1, sizeof(node_t));
-    newNode->data = data;
-    node_t *ptr = start;
-    while (ptr->next != NULL)
-        ptr = ptr->next;
-    ptr->next = newNode;
-    newNode->prev = ptr;
-    newNode->next = NULL;
-    return start;
-}
-
-node_t *insert_before(node_t *start)
-{
-    int data;
-    int search;
-    printf("add data node looking for\t");
-    scanf("%d", &search);
-    printf("add data node\n");
-    scanf("%d", &data);
-    node_t *newNode = (node_t*)calloc(1,sizeof(node_t));
-    node_t *ptr;
-    newNode->data = data;
-    ptr = start;
-    while(ptr->data != search)
-        ptr = ptr->next;
-    newNode->next = ptr;
-    newNode->prev = ptr->prev;
-    ptr->prev->next = newNode;
-    ptr->prev = newNode;
-    return start;
-}
-node_t *insert_after(node_t *start)
-{
-    int data;
-    int search;
-    printf("add data node looking for\t");
-    scanf("%d", &search);
-    printf("add data node\n");
-    scanf("%d", &data);
-    node_t *newNode = (node_t*)calloc(1,sizeof(node_t));
-    node_t *ptr;
-    newNode->data = data;
-    ptr=start;
-    while(ptr->data != search)
-        ptr = ptr->next;
-    newNode->prev = ptr;
-    newNode->next = ptr->next;
-    ptr->next->prev = newNode;
-    ptr->next = newNode;
-    return start;
-}
-
-node_t *delete_first(node_t *start)
-{
-    node_t *ptr = start;
-    start = start->next;
-    start->prev = NULL;
-    free(ptr);
-    return start;
-}
-
-node_t *delete_selected(node_t *start)
-{
-    int data;
-    node_t *ptr, *preptr;
-    ptr = start;
-    printf("enter data of node to delete\n");
-    scanf("%d", &data);
-    while(ptr->data != data)
-        ptr = ptr->next;
-    ptr->next->prev = ptr->prev;
-    ptr->prev->next = ptr->next;
-    free(ptr);
-    return start;
-    
-}
-node_t *delete_last(node_t *start)
-{
-    node_t *ptr = start;
-    node_t *preptr;
-    while(ptr->next != NULL){
-        preptr = ptr;
-        ptr=ptr->next;
-    }
-    preptr->next = NULL;
-    free(ptr);
     return start;
 }
 
@@ -242,7 +83,22 @@ node_t *insert(node_t* start)
 }
 
 node_t *delete(node_t *start){
-
+    node_t *ptr;
+    if(start == NULL){
+        printf("error delete node\n");
+        return start;
+    }
+    else {
+        ptr = start;
+        if(start->next == NULL)
+            free(start);
+        else {
+        start = start->next;
+        start->prev = NULL;
+        free(ptr);
+        }
+        return start;
+    }
 }
 
 node_t *ascending_node(node_t* start)
